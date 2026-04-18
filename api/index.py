795d60f5ca395bd8ebf8a268103ca50193492b2e
nogamate <kurimotoishere@gmail.com>
2026-04-18 21:05:58 +0900

    Auto commit 2026/04/18 21:05:58.73

--- api/index.py
+++ api/index.py
@@ -71,7 +71,7 @@ def callback():
                 elif '杉本' in user_message_clean:
                     keys = ["0", "1"]
                 elif '森ノ宮' in user_message_clean:
-                    keys = ["0", "1"]
+                    keys = ["2", "3"]
                 else:
                     print("DEBUG: No keys matched!") # ここを通るなら文字が一致していません
                     keys = []
